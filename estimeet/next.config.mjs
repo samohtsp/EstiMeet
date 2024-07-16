75f913d9b8fe59dd30b755eaae74272210a587e9
Samohtsp <thomas.richard.gg@gmail.com>
2024-07-16 12:56:37 +0200

    next config vide

--- estimeet/next.config.mjs
+++ estimeet/next.config.mjs
@@ -2,19 +2,19 @@
 const nextConfig = {};
 
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// let assetPrefix = '';
+// let basePath = '';
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
-};
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   trailingSlash: true,
+// };
 export default nextConfig;
