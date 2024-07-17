ef2fa1074978ea876d3faeb7a2548cca9e1fea8e
Samohtsp <thomas.richard.gg@gmail.com>
2024-07-17 21:48:32 +0200

    non optimisation des image et suppression de scripts inutile

--- estimeet/next.config.mjs
+++ estimeet/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/Estimeet' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/Estimeet' : '',
     trailingSlash: true,
-    output: 'export'
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 export default nextConfig;
