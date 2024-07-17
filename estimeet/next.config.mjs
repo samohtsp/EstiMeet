1c5be8cfc30bb275c821c1b40adc266ac7cb219a
Samohtsp <thomas.richard.gg@gmail.com>
2024-07-17 22:35:21 +0200

    ajout du basepath dans github

--- estimeet/next.config.mjs
+++ estimeet/next.config.mjs
@@ -3,8 +3,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+    basePath: process.env.NODE_ENV === 'production' ? process.env.DEPLOYED_GITHUB_PATH : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? process.env.DEPLOYED_GITHUB_PATH : '',
     trailingSlash: true,
     images: {
         unoptimized: true,
